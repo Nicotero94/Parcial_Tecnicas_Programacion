d48d59f8e38b8342d6ccdc48dc8832cc5c1b0188
Nicotero94 <nicolasmgonzalez@hotmail.com>
2017-12-05 20:16:34 -0300

    - Se agrega y modifica unas lineas del ejercicio 3

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,38 +1,51 @@
-def calculadorDelEquipoGanadorDeLaLiga(equipos):
 
-    equipoGanador = []
-
-    if len(equipos) == 0:
-        return ""
+def devolverEquipoMaximoPuntaje(puntajes):
 
 
 
+    tablaDePosiciones = puntajes.items()
 
-    for partidos in range(len(equipos)):
-
-        ganador = []
-
-        for resultado in range(len(equipos[partidos])):
-
-            if equipos[partidos][resultado] != int:
-                equipo = resultado
-            elif equipos[partidos][resultado] == int:
-                goles = resultado
+    for i in tablaDePosiciones:
 
 
+def calculadorDelEquipoGanadorDeLaLiga(resultados):
 
-            print(equiposs)
+    if len(resultados) == 0:
+        return
+
+    EQUIPO_1 = 0
+    EQUIPO_2 = 2
+
+    GOLES_1 = 1
+    GOLES_2 = 3
+
+    puntajes = {}
+
+    for partido in resultados:
+
+        if (partido[GOLES_1] > partido[GOLES_2]):
+
+            ganador = partido[EQUIPO_1]
+            perdedor = partido[EQUIPO_2]
+
+            puntajes[ganador] = 2
+            puntajes[perdedor] = 0
+        elif (partido[GOLES_1] < partido[GOLES_2]):
+
+            ganador = partido[EQUIPO_2]
+            perdedor = partido[EQUIPO_1]
+
+            puntajes[ganador] = 2
+            puntajes[perdedor] = 0
+
+        else:
+            puntajes[partido[EQUIPO_1]] = 1
+            puntajes[partido[EQUIPO_2]] = 1
 
 
 
 
-
-
-
-
-
-
-
+    return devolverEquipoMaximoPuntaje(puntajes)
 
 
 def ejercicio3(var1):

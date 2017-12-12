180890d09a3952b6fcea8ad585b8be92ba83ab2e
Nicotero94 <nicolasmgonzalez@hotmail.com>
2017-12-12 20:26:50 -0300

    -Ejercicio 3 se agregan y modifican algunas lineas. Assert 4 no corre

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,17 +1,41 @@
-def devolverEquipoMaximoPuntaje(puntajes):
+def iniciarTablaDePosicionesConCeros(resultados):
 
-    equipoConPuntajeAlto = max(puntajes.values())
-    equipoGanadorAlfabeticamente = sorted(puntajes.keys())
+    tablaConEquipos = {}
 
-    for equipo, puntos in puntajes.items():
+    for partido in resultados:
 
-        if puntos >= equipoConPuntajeAlto:
-                return equipo
+        diccionario = {partido}
+
+        for equipos in diccionario:
+
+           tablaConEquipos[equipos[0]] = 0
+           tablaConEquipos[equipos[2]] = 0
+
+    return tablaConEquipos
+
+def devolverEquipoMaximoPuntaje(tablaDePosiciones):
+
+    equipoConPuntajeAlto = max(tablaDePosiciones.values())
+    puntosDeLosEquipos = sorted(tablaDePosiciones.values())
+    equipoGanadorAlfabeticamente = sorted(tablaDePosiciones.keys())
+
+    for equipo, puntos in tablaDePosiciones.items():
+
+        lisa = []
+
+        if puntos == equipoConPuntajeAlto:
+            lisa.append(puntos)
+
+
+
+        print(lisa)
 
 def calculadorDelEquipoGanadorDeLaLiga(resultados):
 
     if len(resultados) == 0:
         return ''
+
+    tablaDePosiciones = iniciarTablaDePosicionesConCeros(resultados)
 
     EQUIPO_1 = 0
     EQUIPO_2 = 2
@@ -19,84 +43,43 @@ def calculadorDelEquipoGanadorDeLaLiga(resultados):
     GOLES_1 = 1
     GOLES_2 = 3
 
-    puntajes = {}
-
     for partido in resultados:
 
-        if (partido[GOLES_1] > partido[GOLES_2]):
+        if partido[GOLES_1] > partido[GOLES_2]:
+
             ganador = partido[EQUIPO_1]
             perdedor = partido[EQUIPO_2]
 
             puntoGanador = 2
             puntoPerdedor = 0
 
-            if len(puntajes) == 0:
-                puntajes[ganador] = puntoGanador
-                puntajes[perdedor] = puntoPerdedor
+            puntajeDelEquipoGanador = tablaDePosiciones.get(ganador)
+            puntajeDelEquipoPerdedor = tablaDePosiciones.get(perdedor)
 
-            elif len(puntajes) > 0:
+            tablaDePosiciones[ganador] = puntajeDelEquipoGanador + puntoGanador
+            tablaDePosiciones[perdedor] = puntajeDelEquipoPerdedor + puntoPerdedor
 
-                if ganador not in puntajes:
-                    puntajeDelEquipoGanador = 0
-                else:
-                    puntajeDelEquipoGanador = puntajes.get(ganador)
-
-                if perdedor not in puntajes:
-                    puntajeDelEquipoPerdedor = 0
-                else:
-                    puntajeDelEquipoPerdedor = puntajes.get(perdedor)
-
-                puntajes[ganador] = puntajeDelEquipoGanador + puntoGanador
-                puntajes[perdedor] = puntajeDelEquipoPerdedor + puntoPerdedor
-
-
-        elif (partido[GOLES_1] < partido[GOLES_2]):
+        elif partido[GOLES_1] < partido[GOLES_2]:
             ganador = partido[EQUIPO_2]
             perdedor = partido[EQUIPO_1]
 
             puntoGanador = 2
             puntoPerdedor = 0
 
-            if len(puntajes) == 0:
-                puntajes[ganador] = puntoGanador
-                puntajes[perdedor] = puntoPerdedor
+            puntajeDelEquipoGanador = tablaDePosiciones.get(ganador)
+            puntajeDelEquipoPerdedor = tablaDePosiciones.get(perdedor)
 
-            elif len(puntajes) > 0:
-
-                if ganador not in puntajes:
-                    puntajeDelEquipoGanador = 0
-                else:
-                    puntajeDelEquipoGanador = puntajes.get(ganador)
-
-                if perdedor not in puntajes:
-                    puntajeDelEquipoPerdedor = 0
-                else:
-                    puntajeDelEquipoPerdedor = puntajes.get(perdedor)
-
-                puntajes[ganador] = puntajeDelEquipoGanador + puntoGanador
-                puntajes[perdedor] = puntajeDelEquipoPerdedor + puntoPerdedor
+            tablaDePosiciones[ganador] = puntajeDelEquipoGanador + puntoGanador
+            tablaDePosiciones[perdedor] = puntajeDelEquipoPerdedor + puntoPerdedor
 
         else:
-            if len(puntajes) == 0:
-                puntajes[partido[EQUIPO_1]] = 1
-                puntajes[partido[EQUIPO_2]] = 1
+            equipo1 = partido[EQUIPO_1]
+            equipo2 = partido[EQUIPO_2]
 
-            elif len(puntajes) > 0:
+            tablaDePosiciones[equipo1] += 1
+            tablaDePosiciones[equipo2] += 1
 
-                if EQUIPO_1 not in puntajes:
-                    puntajeDelEquipo1 = 0
-                else:
-                    puntajeDelEquipo1 = puntajes.get(EQUIPO_1)
-
-                if EQUIPO_2 not in puntajes:
-                    puntajeDelEquipo2 = 0
-                else:
-                    puntajeDelEquipo2 = puntajes.get(EQUIPO_2)
-
-                puntajes[partido[EQUIPO_1]] = puntajeDelEquipo1 + 1
-                puntajes[partido[EQUIPO_2]] = puntajeDelEquipo2 + 1
-
-    return devolverEquipoMaximoPuntaje(puntajes)
+    return devolverEquipoMaximoPuntaje(tablaDePosiciones)
 
 def ejercicio3(var1):
     return calculadorDelEquipoGanadorDeLaLiga(var1)
